7483a1fbb6581f86cc2f21b0873ad0edf1acfed9
AleksNik404 <AleksNik404@gmail.com>
2022-12-01 00:39:14 +0300

    feat: solve cssBuilder trash code

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -118,35 +118,120 @@ function fromJSON(proto, json) {
  */
 
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  selectors: [],
+  selectorsOrder: [],
+
+  idIsUse: false,
+  psedoIsUse: false,
+  elemIsUse: false,
+
+  order: ['element', 'id', 'class', 'attr', 'pseudoClass', 'pseudoElement'],
+
+  element(value) {
+    if (this.elemIsUse) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    if (this.isWrongOrder('element', this.selectorsOrder)) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+
+    const selectors = [...this.selectors];
+    const selectorsOrder = [...this.selectorsOrder];
+    const newObj = { ...this, selectors, selectorsOrder };
+
+    newObj.elemIsUse = true;
+    newObj.selectors.push(`${value}`);
+    newObj.selectorsOrder.push('element');
+
+    return newObj;
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    if (this.idIsUse) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    if (this.isWrongOrder('id', this.selectorsOrder)) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+
+    const selectors = [...this.selectors];
+    const selectorsOrder = [...this.selectorsOrder];
+    const newObj = { ...this, selectors, selectorsOrder };
+
+    newObj.idIsUse = true;
+    newObj.selectors.push(`#${value}`);
+    newObj.selectorsOrder.push('id');
+
+    return newObj;
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    if (this.isWrongOrder('class', this.selectorsOrder)) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+
+    const selectors = [...this.selectors];
+    const selectorsOrder = [...this.selectorsOrder];
+    const newObj = { ...this, selectors, selectorsOrder };
+
+    newObj.selectors.push(`.${value}`);
+    newObj.selectorsOrder.push('class');
+
+    return newObj;
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    if (this.isWrongOrder('attr', this.selectorsOrder)) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+
+    const selectors = [...this.selectors];
+    const selectorsOrder = [...this.selectorsOrder];
+    const newObj = { ...this, selectors, selectorsOrder };
+
+    newObj.selectors.push(`[${value}]`);
+    newObj.selectorsOrder.push('attr');
+
+    return newObj;
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    if (this.isWrongOrder('pseudoClass', this.selectorsOrder)) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+
+    const selectors = [...this.selectors];
+    const selectorsOrder = [...this.selectorsOrder];
+    const newObj = { ...this, selectors, selectorsOrder };
+
+    newObj.selectors.push(`:${value}`);
+    newObj.selectorsOrder.push('pseudoClass');
+
+    return newObj;
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    if (this.psedoIsUse) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    if (this.isWrongOrder('pseudoElement', this.selectorsOrder)) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+
+    const selectors = [...this.selectors];
+    const selectorsOrder = [...this.selectorsOrder];
+    const newObj = { ...this, selectors, selectorsOrder };
+
+    newObj.psedoIsUse = true;
+    newObj.selectors.push(`::${value}`);
+    newObj.selectorsOrder.push('pseudoElement');
+
+    return newObj;
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine(selector1, combinator, selector2) {
+    const selectors = [...selector1.selectors, ` ${combinator} `, ...selector2.selectors];
+    return { ...selector1, ...selector2, selectors };
+    // return this;
+  },
+
+  stringify() {
+    return this.selectors.join('');
+  },
+
+  isWrongOrder(selector, curOrder) {
+    const indexSelector = this.order.indexOf(selector);
+    const falsyOrder = this.order.slice(indexSelector + 1);
+
+    if (curOrder.some((el) => falsyOrder.includes(el))) {
+      return true;
+    }
+
+    return false;
   },
 };
-
 
 module.exports = {
   Rectangle,
